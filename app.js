042fa7e482294fa813ed36b9f06ae6f144ef4769
Isha Guravaleh <isha.xornor@gmail.com>
2022-10-04 16:07:08 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get("/", function (req, res) {
+    
+    var today = new Date();
+
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    var day = today.toLocaleDateString("en-US", options);
+
+    res.render("list", { kindofDay: day });
+});
+
+
+app.listen(4000, function () {
+    console.log("Server started on port 4000");
+});
